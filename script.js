a81699fdbd3f6b3ae2c9e557989cd5d4ea165336
Power_tile <hejizheng2004@163.com>
2020-05-21 13:37:26 +0800

    add author part to html and js file

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ let app = new Vue({
         projects: [ // projects here
             {
                 name: "Project 1",
+                author: "Author 1\nAuthor2",
                 description: "Description 1"
             },
             {
                 name: "Project 2",
+                author: "Author3\nAuthor4\nAuthor5",
                 description: "Description 2"
             }
         ],
