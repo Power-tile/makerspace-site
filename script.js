f169ce169d7063458aa20014d45dfd81bd7cdf93
Power_tile <hejizheng2004@163.com>
2020-05-21 13:29:00 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        projects: [ // projects here
+            {
+                name: "Project 1",
+                description: "Description 1"
+            },
+            {
+                name: "Project 2",
+                description: "Description 2"
+            }
+        ],
+        people: [
+            {
+                name: "Leadership Group"
+            }
+        ]
+    }
+});
